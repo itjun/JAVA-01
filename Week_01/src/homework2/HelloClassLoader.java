744fd46ecb295c5ca6d80523fa19ab72978c5344
IT君 <l1091462907@gmail.com>
2021-01-15 00:31:49 +0800

    add homework2

---
+++ Week_01/src/homework2/HelloClassLoader.java
@@ -0,0 +1,42 @@
+package homework2;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class HelloClassLoader extends ClassLoader {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException,
+            InstantiationException, NoSuchMethodException, InvocationTargetException {
+        // 获取 Hello 这个类
+        Class clazz = new HelloClassLoader().findClass("Hello");
+        // 获取 hello 方法
+        Method method = clazz.getDeclaredMethod("hello");
+        // 调用 hello 方法
+        method.invoke(clazz.newInstance());
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File file = new File("Hello.xlass");
+        byte[] data = new byte[(int) file.length()];
+        try {
+            DataInputStream inputStream = new DataInputStream(new FileInputStream(file));
+            inputStream.readFully(data);
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        decode(data);
+        return defineClass(name, data, 0, data.length);
+    }
+
+    private void decode(byte[] data) {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (byte) (255 - data[i]);
+        }
+    }
+}
