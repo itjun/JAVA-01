791ab631e83ba767a55266d8b6c5b60961c7aa79
IT君 <l1091462907@gmail.com>
2021-02-08 17:11:19 +0800

    Create Student.java

---
+++ Week_05/homework/src/main/java/io/itjun/week05/bean/Student.java
@@ -0,0 +1,16 @@
+package io.itjun.week05.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+
+    private int id;
+    private String name;
+    private String clazz;
+
+}
